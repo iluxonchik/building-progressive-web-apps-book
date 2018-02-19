771bb47f28d513f9da52353582df2c6d53b09969
ILUXONCHIK <iluxon4ik@hotmail.com>
2018-02-19 06:18:08 +0000

    add simple caching SV example

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,3 +1,19 @@
+/** Section for temp code used as example **/
+
+function bootstrapInterceptSV(event) {
+  if (event.request.url.includes("bootstrap.min.css")) {
+    event.respondWith(
+      new Response(
+        ".hotel-slogan {background: green!important;} nav {display:none}",
+        { headers: { "Content-Type": "text/css"}}
+      )
+    );
+  }
+}
+
+/** **/
+
+
 // add event listener to the Service Worker. "self" refers to the service worker
 // itself. All "fetch" requests are now being intercepted
 self.addEventListener("fetch", function(event){
